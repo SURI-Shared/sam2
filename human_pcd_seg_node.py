1009ffa7c0899f60cf95edecab06e0389015f10b
medusaL <jinye1514@gmail.com>
2025-03-24 19:21:43 -0400

    updated to use a separate rs_aligned_depth.launch node for realsense interfacing

--- human_pcd_seg_node.py
+++ human_pcd_seg_node.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
-from tools.realsense import RealSensePointCloudGenerator
+from tools import realsense
 import cv2
 import open3d
 from tools import data_logging
@@ -14,15 +14,20 @@ import os
 
 from gpd_ros.msg import CloudIndexed,CloudSources
 from std_msgs.msg import Header
-from sensor_msgs.msg import PointCloud2,PointField
+from sensor_msgs.msg import PointCloud2,PointField,CameraInfo
+from sensor_msgs.msg import Image as ImageMsg
 from geometry_msgs.msg import TransformStamped,Transform
 import sensor_msgs.point_cloud2 as pc2
 import tf2_msgs.msg
 import tf2_ros
 from scipy.spatial.transform import Rotation
+from threading import Lock
+from cv_bridge import CvBridge
 
 camera_frame="camera_depth_optical_frame"
 fixed_frame="world"
+depth_scale=1e-3#hardcoded value of mm per https://github.com/IntelRealSense/realsense-ros/issues/277#issuecomment-525676873
+bridge=CvBridge()
 
 def transform_msg_to_homogeneous(msg:Transform):
     rotation=Rotation.from_quat([msg.rotation.x,msg.rotation.y,msg.rotation.z,msg.rotation.w])
@@ -33,6 +38,39 @@ def transform_msg_to_homogeneous(msg:Transform):
     return mat
 
 rospy.init_node("human_in_loop_segmentation")
+
+image_lock=Lock()
+
+color_msg=None
+depth_msg=None
+
+def record_color_image(msg:ImageMsg):
+    global color_msg
+    with image_lock:
+        color_msg=msg
+
+def record_depth_image(msg:ImageMsg):
+    global depth_msg
+    with image_lock:
+        depth_msg=msg
+
+depth_info_msg=None
+def record_depth_info(msg:CameraInfo):
+    global depth_info_msg
+    with image_lock:
+        depth_info_msg=msg
+
+color_info_msg=None
+def record_color_info(msg:CameraInfo):
+    global color_info_msg
+    with image_lock:
+        color_info_msg=msg
+
+depth_sub=rospy.Subscriber("/camera/aligned_depth_to_color/image_raw",ImageMsg,record_depth_image)
+color_sub=rospy.Subscriber("/camera/color/image_raw",ImageMsg,record_color_image)
+depth_info_sub=rospy.Subscriber("/camera/aligned_depth_to_color/camera_info",CameraInfo,record_depth_info)
+depth_info_sub=rospy.Subscriber("/camera/color/camera_info",CameraInfo,record_color_info)
+
 cloud_pub=rospy.Publisher("/cloud_stitched",PointCloud2,latch=True)
 full_cloud_pub=rospy.Publisher("/raw_cloud",PointCloud2,latch=True)
 pub_tf = rospy.Publisher("/tf", tf2_msgs.msg.TFMessage, queue_size=1)
@@ -59,25 +97,28 @@ model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml"
 sam2 = build_sam2(model_cfg, sam2_checkpoint, device=device, apply_postprocessing=False)
 predictor = SAM2ImagePredictor(sam2)
 
-camera=RealSensePointCloudGenerator(background_distance)
 got_image=False
+depth_intrinsic_mat=None
 print("press 'c' to capture image or 'q' to quit")
 while not got_image:
-    timestamp,color,depth=camera.wait_for_rgbd()
-    color_array=np.asanyarray(color.get_data())
-    cv2.imshow("Live",color_array)
-    if cv2.waitKey(10)==ord("c"):
-        got_image=True
-        camera.stop_capture()
-        print('Image captured')
-    if cv2.waitKey(10)==ord('q'):
-        camera.stop_capture()
-        print("Quitting")
-        exit()
+    with image_lock:
+        if color_msg is not None:
+            #TODO: ensure depth and color images are actually synchronized
+            color_mat=cv2.cvtColor(bridge.imgmsg_to_cv2(color_msg),cv2.COLOR_RGB2BGR)
+            depth_mat=bridge.imgmsg_to_cv2(depth_msg)
+            cv2.imshow("Live",color_mat)
+            if cv2.waitKey(10)==ord("c"):
+                got_image=True
+                depth_intrinsic_mat=np.array(depth_info_msg.K).reshape((3,3))
+                print('Image captured')
+            if cv2.waitKey(10)==ord('q'):
+                print("Quitting")
+                exit()
 
 cv2.destroyAllWindows()
-image=Image.fromarray(color_array)
-image = np.array(image.convert("RGB"))
+color_rgb=cv2.cvtColor(color_mat,cv2.COLOR_BGR2RGB)
+image=Image.fromarray(color_rgb)
+# image = np.array(image.convert("RGB"))
 
 predictor.set_image(image)
 
@@ -139,15 +180,16 @@ scores = scores[sorted_ind]
 logits = logits[sorted_ind]
 
 masked_image=add_mask(ax, masks[0], point_coords=np.array(pt), input_labels=np.array([item_idx],dtype=np.int64), borders=True)
+depth_scaled=(depth_mat*depth_scale).astype(np.float32)
 
-raw_depth=camera.depth_array_from_depth_frame(depth)
+rgbd=realsense.o3d_rgbd_from_color_and_depth_arrays(masked_image,depth_scaled,False)
+o3dpcd=open3d.t.geometry.PointCloud.create_from_rgbd_image(rgbd, open3d.core.Tensor(depth_intrinsic_mat),depth_scale=1,depth_max=background_distance)
 
-o3dpcd=camera.o3dpcd_from_color_and_depth_arrays(masked_image,raw_depth,False)
-
-masked_depth=raw_depth.copy()
+masked_depth=depth_scaled.copy()
 bool_mask=masks[0].astype(np.bool)
 masked_depth[np.logical_not(bool_mask)]=2*background_distance
-segmented_pcd=camera.o3dpcd_from_color_and_depth_arrays(masked_image,masked_depth,False)
+masked_rgbd=realsense.o3d_rgbd_from_color_and_depth_arrays(masked_image,masked_depth,False)
+segmented_pcd=open3d.t.geometry.PointCloud.create_from_rgbd_image(masked_rgbd, open3d.core.Tensor(depth_intrinsic_mat),depth_scale=1,depth_max=background_distance)
 all_points_in_camera=o3dpcd.point.positions.numpy()
 segmented_points_in_camera=segmented_pcd.point.positions.numpy()
 
